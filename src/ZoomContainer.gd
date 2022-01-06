232d6e39fc19591f396648f74950340740917eea
tijmenvangulik <tijmen@vangulik.org>
2022-01-06 20:57:46 +0100

    Animate zoom

--- src/ZoomContainer.gd
+++ src/ZoomContainer.gd
@@ -10,13 +10,35 @@ extends HBoxContainer
 #func _process(delta):
 #	pass
 
+var newZoom=-1
+var step=0.5
+
+func directSetZoom(zoom):
+	var camera=$"../../../Boat/Camera2D2"
+	camera.zoom.x=zoom
+	camera.zoom.y= zoom
+	newZoom=-1
+	$"../../../Boat".setForwardsPosition(0)
+	
+func _process(delta):
+	if newZoom!=-1:
+		var camera=$"../../../Boat/Camera2D2"
+		if camera.zoom.x< newZoom:
+			camera.zoom.x+=delta*step
+			camera.zoom.y= camera.zoom.x
+			if camera.zoom.x>newZoom:
+				directSetZoom(newZoom)
+		
+		if camera.zoom.x> newZoom:
+			camera.zoom.x-=delta*step
+			camera.zoom.y= camera.zoom.x
+			if camera.zoom.x<newZoom:
+				directSetZoom(newZoom)
+		
 func zoom(delta):
 	var camera=$"../../../Boat/Camera2D2"
-	var newZoom=camera.zoom.x+delta*0.1
+	newZoom=camera.zoom.x+delta*0.1
 	if newZoom>0 && newZoom<100:
-		camera.zoom.x= newZoom
-		camera.zoom.y= newZoom
-		$"../../../Boat".setForwardsPosition(0)
 		$"../../SettingsDialog".saveSettings()
 
 func _on_ZoomPlus_pressed():
