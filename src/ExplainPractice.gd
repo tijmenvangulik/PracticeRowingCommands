579e6c8a9ac1488fa36929905eacf167edaacb57
tijmenvangulik <tijmen@vangulik.org>
2022-12-29 13:35:47 +0100

    3.5.1 english translations and make switch language possible in explain dialog

--- src/ExplainPractice.gd
+++ src/ExplainPractice.gd
@@ -1,14 +1,24 @@
 extends WindowDialog
 
+var _explainText=""
+
 func _ready():
 	get_close_button().hide()
-
+	GameEvents.connect("languageChangedSignal",self,"_on_languageChanged")
+	
 # Called when the node enters the scene tree for the first time.
 func showDialog(explainText):
-	var text=tr(explainText);
-	if text!=null && text!="" && text!=explainText :
+	_explainText=explainText
+	showText()
+	
+func showText():
+	var text=tr(_explainText);
+	if text!=null && text!="" && text!=_explainText :
 		$PracticeExplain.set_bbcode(Utilities.replaceCommandsInText(text,true))
 		visible=true
 	
 func _on_Start_pressed():
 	hide()
+
+func _on_languageChanged():
+	showText()
