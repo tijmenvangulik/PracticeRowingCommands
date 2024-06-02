3ef8b16a4ad4b80b42a90dbc6a3deb1c3facac06
tijmenvangulik <tijmen@vangulik.org>
2024-06-02 16:17:20 +0200

    4.0.3 let it shine

--- src/Collectable.gd
+++ src/Collectable.gd
@@ -13,28 +13,43 @@ var isEntered=false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+var slowTween= [1,1.4]
 
-		
+func startSlowTween():
+	$TweenSlow.interpolate_property($SpriteTransparent, "scale", Vector2(slowTween[0],slowTween[0]), Vector2(slowTween[1],slowTween[1]), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	
+	$TweenSlow.start()
+
+func _on_TweenSlow_tween_all_completed():
+	slowTween.invert()
+	startSlowTween()
+	
 func _on_Tween_tween_all_completed():
 	visible=false
 
 func reset():
+	$Tween.stop_all()
+	$TweenSlow.stop_all()
 	visible=true
 	monitoring=true
 	var sprite=$Sprite
 	sprite.scale= Vector2(1,1)
 	sprite.modulate=Color(1, 1, 1, 1)
+	$SpriteTransparent.modulate=Color(1, 1, 1, 0.5)
 	collected=false
 	isEntered=false
+	startSlowTween()
 	
 func hide():
 	visible=false
 	monitoring=false
 	isEntered=false
+	$TweenSlow.stop_all()
 
 func doCollect():
 	monitoring=false
 	collected=true
+	$TweenSlow.stop_all()
 	var scaleUp=4
 	$"..".collect(1)
 	$Tween.interpolate_property($Sprite, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
@@ -58,5 +73,7 @@ func _process(delta):
 		if self.visible && !collected && isEntered && triggerWhenBoatIsStopped && $"%Boat".isStopped() && $"%Boat".state==Constants.RowState.Bedankt :
 			doCollect()
 		
+
+
 
 
