ef5be00dd734eb28ae3ea21d6d8bc28d6c6cfce3
tijmenvangulik <tijmen@vangulik.org>
2024-10-29 13:16:37 +0100

    5.6.0 more friendly url sharing when supported

--- src/ShareSettingsDialog.gd
+++ src/ShareSettingsDialog.gd
@@ -9,11 +9,19 @@ func setSettingInUrl():
 		urlLang=Constants.urlKeys[indexNr]
 	var settings=$"%SettingsDialog".getSettings(true)
 	var urlSettings= to_json(settings).percent_encode()
-	JavaScript.eval("history.pushState({}, null, window.location.protocol + '//' + window.location.host + window.location.pathname + '?lang="+urlLang+"&settings="+urlSettings+"')");
+	var hasShare=JavaScript.eval("typeof navigator.share=='function'");
+	var urlScript="window.location.protocol + '//' + window.location.host + window.location.pathname + '?lang="+urlLang+"&settings="+urlSettings+"'"
+	if hasShare:
+		JavaScript.eval("navigator.share({url:"+urlScript+"})");
+		return false
+	else:
+		JavaScript.eval("history.pushState({}, null, "+urlScript+")");
+		JavaScript.eval("if (typeof navigator.clipboard=='object' && typeof navigator.clipboard.writeText=='function' ) navigator.clipboard.writeText("+urlScript+");")
+	return true
 
 func start():
-	setSettingInUrl()
-	show_modal(true)
+	if setSettingInUrl():
+		show_modal(true)
 
 
 func _on_CloseDialog_pressed():
