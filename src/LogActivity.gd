71f1ef3ee6f58faf3c95efe4b072f73f0af2836f
tijmenvangulik <tijmen@vangulik.org>
2025-06-04 19:27:10 +0200

    6.8.4 log real name of custom language

--- src/LogActivity.gd
+++ src/LogActivity.gd
@@ -6,11 +6,17 @@ extends HTTPRequest
 # anonymous loging some usage info
 # log when a practice is finished with success or failed
 func logFinishedActivity(name : String, success : bool,isScull : bool):
-	
+	var lang=Settings.currentLang
+	if lang==Languages.sharedSettingLangKey:
+		if Settings.sharedSettings!=null && Settings.sharedSettings.has("name"):
+			var settingsName=Settings.sharedSettings.get("name")
+			if settingsName!="":
+				lang=settingsName
+		
 	var url=Constants.serverUrl+"/logFinishedActivity"
 	#var url="http://localhost:2024/logFinishedActivity"
 	url=url+"?data[name]="+name.percent_encode()
-	url=url+"&data[language]="+Settings.currentLang
+	url=url+"&data[language]="+lang.percent_encode()
 	url=url+"&data[success]="+Utilities.boolToSting(success)
 	url=url+"&data[isScull]="+Utilities.boolToSting(isScull)
 	$"%LogActivityRequest".request(url, [], true, HTTPClient.METHOD_GET)
